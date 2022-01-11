2d31551246a38cc7528242cb7c0f8ef02db02da0
MichaelKinsella <michael@kingswood.ie>
2022-01-11 18:30:31 +0000

    V6.0 R3.7
    Brought over Bug Fix from V6.0 R3.7 for the uMedicineSetup screen.

--- GenTypesConst.pas
+++ GenTypesConst.pas
@@ -68,8 +68,8 @@ uses
    SysUtils, Messages, Graphics, Windows, FileCtrl;
 
 const
-   HerdVerNo = 'KHM V6.0 R3.5';
-   iHerdVerNo = 6035;
+   HerdVerNo = 'KHM V6.0 R3.7';
+   iHerdVerNo = 6037;
 
    //*** All Herd Version Changes moved to Changes unit.***//
 
